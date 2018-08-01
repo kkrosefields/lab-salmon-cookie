026b43e39256cbe7031832bdf670cfbe4ad4bcf2
kkrosefields <kkrosefields@gmail.com>
2018-08-01 08:55:18 -0700

    got tables to work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,68 @@
 
+'use strict';
+
+(function(module){
+    var stores = module.stores;
+    var toDom = module.toDom;
+    var html = module.html;
+
+    var td = document.getElementById('stores-id');
+
+    var render = function(stores) {
+        return toDom(html`  
+            
+    <tr>
+        <th>location</th>
+        <th>6am</th>
+        <th>7am</th>
+        <th>8am</th>
+        <th>9am</th>
+        <th>10am</th>
+        <th>11m</th>
+        <th>12pm</th>
+        <th>1pm</th>
+        <th>2pm</th>
+        <th>3pm</th>
+        <th>4pm</th>
+        <th>5pm</th>
+        <th>6pm</th>
+        <th>7pm</th>
+        <th>8pm</th>
+    </tr>
+
+    <tr>
+        <td>${stores.name}</td>
+        <td>${stores.hours[0]}</td>
+        <td>${stores.hours[1]}</td>
+        <td>${stores.hours[2]}</td>
+        <td>${stores.hours[3]}</td>
+        <td>${stores.hours[4]}</td>
+        <td>${stores.hours[5]}</td>
+        <td>${stores.hours[6]}</td>
+        <td>${stores.hours[7]}</td>
+        <td>${stores.hours[8]}</td>
+        <td>${stores.hours[9]}</td>
+        <td>${stores.hours[10]}</td>
+        <td>${stores.hours[11]}</td>
+        <td>${stores.hours[12]}</td>
+        <td>${stores.hours[13]}</td>    
+    </tr>
+
+    
+
+        `); 
+
+    }; 
+
+    var store;
+    for(var i = 0; i < stores.length; i++) {
+        store = stores[i];
+        var dom = render(store);
+
+        td.appendChild(dom);
+
+        console.log('store one hours', store);
+    } 
+
+})(window.module = window.module || {});
+
--- js/stores.js
+++ js/stores.js
@@ -6,28 +6,28 @@
         name:'Pike Place Market',
         min:'23',
         max:'65',
-        avg:'6.3',
+        avg:'6',
         key:'pike'
     }, {
 
         name:'SeaTac',
         min:'3',
         max:'24',
-        avg:'1.2',
+        avg:'1',
         key:'seatac',
     }, {
 
         name:'Pike Place Market',
         min:'11',
         max:'38',
-        avg:'3.7',
+        avg:'3',
         key:'seattlecenter'
     }, {
 
         name: 'Capitol Hill',
         min:'20',
         max:'38',
-        avg:'2.3',
+        avg:'2',
         key:'caphill'
     }, 
 
@@ -35,28 +35,31 @@
         name: 'Alki',
         min:'2',
         max:'16',
-        avg:'4.6',
+        avg:'6',
         key:'alki' 
     
     }];
+
     module.stores = stores;
 
+    for(var i = 0; i < stores.length; i++){
+        stores[i].hours = [];
+        for(var j = 0; j < 14; j++) {
+            var hour = stores[i].hours;
+            var cookiesPerhour = getCustomerPerHour(stores[i].min, stores[i].max * stores[i].avg);
+            hour.push(cookiesPerhour);
+            //console.log('cookies per hour', cookiesPerhour);
+        }
+    }
+    function getCustomerPerHour(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    
 
 })(window.module = window.module || {});
 
 
-for(var i = 0; i < window.module.stores.length; i++){
-    window.module.stores[i].hours = [];
-    for(var j = 0; j < 14; j++) {
-        var hour = window.module.stores[i].hours;
-        var cookiesPerhour = getCustomerPerHour(window.module.stores[i].min, window.module.stores[i].max * window.module.stores[i].avg);
-        hour.push(cookiesPerhour);
-        console.log('cookies per hour', cookiesPerhour);
-    }
-}
-function getCustomerPerHour(min, max) {
-    return Math.random() * (max - min) + min;
-}
+
 
 
 
